c32c69f7c2a14c51f1dd239ad991d026add12a66
Dmytro Shchoma <dmytroshchoma@gmail.com>
2025-01-15 14:40:15 +0200

    Solution

--- src/routes/movies.py
+++ src/routes/movies.py
@@ -1,10 +1,47 @@
 from fastapi import APIRouter, Depends, HTTPException, Query
 from sqlalchemy.orm import Session
+from typing import Optional
 
 from database import get_db, MovieModel
-
+from schemas.movies import MovieDetailResponseSchema, MovieListResponseSchema
 
 router = APIRouter()
 
 
-# Write your code here
+@router.get("/movies/", response_model=MovieListResponseSchema)
+def get_movies(
+        db: Session = Depends(get_db),
+        page: int = Query(1, ge=1),
+        per_page: int = Query(10, ge=1, le=20),
+):
+    query = db.query(MovieModel)
+
+    total_items = query.count()
+    total_pages = (total_items + per_page - 1) // per_page
+
+    if page > total_pages:
+        raise HTTPException(status_code=404, detail="No movies found.")
+
+    movies_query = query.offset((page - 1) * per_page).limit(per_page).all()
+    movies = [MovieDetailResponseSchema.model_validate(movie) for movie in movies_query]
+
+    prev_page = f"/theater/movies/?page={page - 1}&per_page={per_page}" if page > 1 else None
+    next_page = f"/theater/movies/?page={page + 1}&per_page={per_page}" if page < total_pages else None
+
+    return MovieListResponseSchema(
+        movies=movies,
+        prev_page=prev_page,
+        next_page=next_page,
+        total_pages=total_pages,
+        total_items=total_items
+    )
+
+
+@router.get("/movies/{movie_id}/", response_model=MovieDetailResponseSchema)
+def get_movie(movie_id: int, db: Session = Depends(get_db)):
+    movie = db.query(MovieModel).filter(MovieModel.id == movie_id).first()
+
+    if not movie:
+        raise HTTPException(status_code=404, detail="Movie with the given ID was not found.")
+
+    return movie
