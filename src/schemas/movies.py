5c090314b4d81fc686c9f975bb5279ccdc35cb71
Max <mxbely98@gmail.com>
2025-01-17 11:31:03 +0200

    implement schemas and routes

--- src/schemas/movies.py
+++ src/schemas/movies.py
@@ -1,1 +1,27 @@
-# Write your code here
+from pydantic import BaseModel
+import datetime
+from typing import Optional, List
+
+
+class MovieDetailResponseSchema(BaseModel):
+    id: int
+    name: str
+    date: datetime.date
+    score: float
+    genre: str
+    overview: str
+    crew: str
+    orig_title: str
+    status: str
+    orig_lang: str
+    budget: float
+    revenue: float
+    country: str
+
+class MovieListResponseSchema(BaseModel):
+    movies: List[MovieDetailResponseSchema]
+    prev_page: Optional[str] | None
+    next_page: Optional[str] | None
+    total_pages: int
+    total_items: int
+
