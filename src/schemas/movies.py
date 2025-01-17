4ad669e5b1fd046210c7807a6897448b196f4f3a
user <nikitaskrypets@gmail.com>
2025-01-17 15:47:29 +0200

    implemented schemas

--- src/schemas/movies.py
+++ src/schemas/movies.py
@@ -1,1 +1,28 @@
-# Write your code here
+import datetime
+
+from pydantic import BaseModel, ConfigDict
+
+
+class MovieSchema(BaseModel):
+    id: int
+    name: str
+    date: datetime.date
+    score: float
+    genre: str
+    overview: str
+    crew: str
+    orig_title: str
+    status: str
+    orig_lang: str
+    budget: float
+    revenue: float
+    country: str
+    model_config = ConfigDict(from_attributes=True)
+
+
+class PaginatedMovies(BaseModel):
+    movies: list[MovieSchema]
+    prev_page: str | None
+    next_page: str | None
+    total_pages: int
+    total_items: int
