bae2368c11088d983ce89c66a854c38ba84ed471
roman <skyfoxwork@gmail.com>
2025-01-17 14:36:59 +0200

    Solution-py-fastapi-homework-1-task

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 
 from routes import movie_router
+# from routes import movies
 
 app = FastAPI(
     title="Movies homework",
@@ -10,3 +11,4 @@ app = FastAPI(
 api_version_prefix = "/api/v1"
 
 app.include_router(movie_router, prefix=f"{api_version_prefix}/theater", tags=["theater"])
+# app.include_router(movies.router)
