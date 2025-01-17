736471cd65fb3b574aa15f7301ccfcf8237e4457
roman <skyfoxwork@gmail.com>
2025-01-17 14:45:54 +0200

    fix

--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 
 from routes import movie_router
-# from routes import movies
 
 app = FastAPI(
     title="Movies homework",
@@ -11,4 +10,3 @@ app = FastAPI(
 api_version_prefix = "/api/v1"
 
 app.include_router(movie_router, prefix=f"{api_version_prefix}/theater", tags=["theater"])
-# app.include_router(movies.router)
