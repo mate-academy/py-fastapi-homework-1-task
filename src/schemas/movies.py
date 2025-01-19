abe9207f1647e287c695ef526115c303379a92e5
94nj111 <94nj111@gmail.com>
2025-01-19 16:19:36 +0200

    fastapi-homework-1-task solution

--- src/schemas/movies.py
+++ src/schemas/movies.py
@@ -1,1 +1,30 @@
-# Write your code here
+from typing import List, Optional
+
+from pydantic import BaseModel, ConfigDict
+from datetime import date
+
+
+class MovieDetailResponseSchema(BaseModel):
+    id: int
+    name: str
+    date: date
+    score: int
+    genre: str
+    overview: str
+    crew: str
+    orig_title: str
+    status: str
+    orig_lang: str
+    budget: float
+    revenue: float
+    country: str
+
+    model_config = ConfigDict(from_attributes=True)
+
+
+class MovieListResponseSchema(BaseModel):
+    movies: List[MovieDetailResponseSchema]
+    prev_page: Optional[str]
+    next_page: Optional[str]
+    total_pages: int
+    total_items: int
--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -6,8 +6,8 @@ from database import (
     reset_sqlite_database,
     get_db_contextmanager,
 )
-from database.populate import CSVDatabaseSeeder
-from main import app
+from src.database.populate import CSVDatabaseSeeder
+from src.main import app
 
 
 @pytest.fixture(scope="function", autouse=True)
@@ -30,7 +30,9 @@ def db_session():
 @pytest.fixture(scope="function")
 def seed_database(db_session):
     settings = get_settings()
-    seeder = CSVDatabaseSeeder(csv_file_path=settings.PATH_TO_MOVIES_CSV, db_session=db_session)
+    seeder = CSVDatabaseSeeder(
+        csv_file_path=settings.PATH_TO_MOVIES_CSV, db_session=db_session
+    )
     if not seeder.is_db_populated():
         seeder.seed()
     yield db_session
