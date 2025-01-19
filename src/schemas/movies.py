4797d39e22204f4ec175bbd642708d505594b076
Vladyslav Kucherenko <vlados.soc@gmail.com>
2025-01-19 12:47:20 +0200

    Solution

--- src/schemas/movies.py
+++ src/schemas/movies.py
@@ -1,1 +1,25 @@
-# Write your code here
+from datetime import date
+
+from pydantic import BaseModel
+
+
+class MovieDetailResponseSchema(BaseModel):
+    id: int
+    name: str
+    date: date
+    score: float
+    genre: str
+    overview: str
+    crew: str
+    orig_title: str
+    status: str
+    orig_lang: str
+    budget: float
+    revenue: float
+    country: str
+
+    model_config = {"from_attributes": True}
+
+
+class MovieListResponseSchema(MovieDetailResponseSchema):
+    pass
