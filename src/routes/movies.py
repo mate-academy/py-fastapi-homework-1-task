8ff3004c16e5c6b0cf2716efe0ec85d322638e21
Соня Cавкова <silva.vvss12@gmail.com>
2025-01-19 19:16:28 +0200

    solution

--- src/routes/movies.py
+++ src/routes/movies.py
@@ -1,10 +1,51 @@
 from fastapi import APIRouter, Depends, HTTPException, Query
 from sqlalchemy.orm import Session
 
+from schemas.movies import MovieListResponseSchema, MovieDetailResponseSchema
+
 from database import get_db, MovieModel
 
 
 router = APIRouter()
 
 
-# Write your code here
+@router.get("/movies/", response_model=MovieListResponseSchema)
+def get_movies(
+        page: int = Query(1, ge=1),
+        per_page: int = Query(10, le=20, ge=1),
+        db: Session = Depends(get_db)
+) -> MovieListResponseSchema:
+    total_items = db.query(MovieModel).count()
+    total_pages = (total_items + per_page - 1) // per_page
+
+    offset = (page - 1) * per_page
+    movies = db.query(MovieModel).offset(offset).limit(per_page).all()
+
+    if not movies:
+        raise HTTPException(status_code=404, detail="No movies found.")
+
+    prev_page = (f"/theater/movies/?page={page - 1}"
+                 f"&per_page={per_page}") if page > 1 else None
+    next_page = (f"/theater/movies/?page={page + 1}"
+                 f"&per_page={per_page}") if page < total_pages else None
+
+    return MovieListResponseSchema(
+        movies=movies,
+        prev_page=prev_page,
+        next_page=next_page,
+        total_pages=total_pages,
+        total_items=total_items
+    )
+
+
+@router.get("/movies/{movie_id}", response_model=MovieDetailResponseSchema)
+def get_movie_details(movie_id: int, db: Session = Depends(get_db)) -> MovieDetailResponseSchema:
+    movie = db.query(MovieModel).filter(MovieModel.id == movie_id).first()
+
+    if not movie:
+        raise HTTPException(
+            status_code=404,
+            detail="Movie with the given ID was not found."
+        )
+
+    return MovieDetailResponseSchema.model_validate(movie)
--- src/schemas/movies.py
+++ src/schemas/movies.py
@@ -1,1 +1,29 @@
-# Write your code here
+from pydantic import BaseModel, ConfigDict
+from typing import List
+import datetime
+
+
+class MovieDetailResponseSchema(BaseModel):
+    id: int
+    name: str
+    date: datetime.date
+    score: float
+    genre: str
+    overview: str
+    crew: str
+    orig_title: str
+    status: str
+    orig_lang: str
+    budget: float
+    revenue: float
+    country: str
+
+    model_config = ConfigDict(from_attributes=True)
+
+
+class MovieListResponseSchema(BaseModel):
+    movies: List[MovieDetailResponseSchema]
+    prev_page: str | None = None
+    next_page: str | None = None
+    total_pages: int
+    total_items: int
