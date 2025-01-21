c61aa60ae371c72838b32406b6cceaf47318186f
Max Lapchuk <makslapchuk@gmail.com>
2025-01-21 17:04:43 +0200

    Fix v2

--- src/routes/movies.py
+++ src/routes/movies.py
@@ -25,8 +25,8 @@ def get_movies(
 
     total_pages = math.ceil(total_items / per_page)
 
-    prev_page = f"/movies/?page={page - 1}&per_page={per_page}" if page > 1 else None
-    next_page = f"/movies/?page={page + 1}&per_page={per_page}" if page < total_pages else None
+    prev_page = f"/theater/movies/?page={page - 1}&per_page={per_page}" if page > 1 else None
+    next_page = f"/theater/movies/?page={page + 1}&per_page={per_page}" if page < total_pages else None
 
     response = {
         "movies": movies,
