3922ea92f4e1e1999ec7732c70b9a95997866e74
Mykola <jacktyler2391@gmail.com>
2025-01-24 13:11:03 +0200

    Solution

--- src/main.py
+++ src/main.py
@@ -1,12 +1,15 @@
 from fastapi import FastAPI
+from fastapi_pagination import add_pagination
 
-from routes import movie_router
+from src.routes import movie_router
 
 app = FastAPI(
     title="Movies homework",
     description="Description of project"
 )
+add_pagination(app)
 
 api_version_prefix = "/api/v1"
+
 
 app.include_router(movie_router, prefix=f"{api_version_prefix}/theater", tags=["theater"])
--- src/routes/movies.py
+++ src/routes/movies.py
@@ -1,10 +1,27 @@
 from fastapi import APIRouter, Depends, HTTPException, Query
 from sqlalchemy.orm import Session
 
-from database import get_db, MovieModel
+from src.database import get_db, MovieModel
+from src.schemas.movies import MovieListResponseSchema, MovieDetailResponseSchema
 
+from fastapi_pagination import Page, paginate
 
 router = APIRouter()
 
 
-# Write your code here
+@router.get("/movies/", response_model=Page[MovieListResponseSchema])
+def get_paginated_movies(db: Session = Depends(get_db), page: int = 1, size: int = 10):
+
+    movies = db.query(MovieModel).all()
+    start = (page - 1) * size
+    end = start + size
+
+    return paginate(movies[start:end])
+
+
+@router.get("/movies/{movie_id}/", response_model=MovieDetailResponseSchema)
+def get_detail_movie(movie_id: int, db: Session = Depends(get_db)):
+    movie = db.query(MovieModel).filter(MovieModel.id == movie_id).first()
+    if not movie:
+        raise HTTPException(status_code=404, detail="Movie not found")
+    return movie
