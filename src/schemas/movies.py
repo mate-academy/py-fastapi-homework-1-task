f8a782d41f37b7776d5babcc99a3b1e0fa549153
TarasFirst <firsttaras@gmail.com>
2025-01-24 09:10:31 +0100

    implement pagination class

--- src/schemas/movies.py
+++ src/schemas/movies.py
@@ -1,1 +1,14 @@
 # Write your code here
+from typing import TypeVar, Generic, List
+from pydantic import BaseModel, conint, GenericModel
+T = TypeVar("T")
+class PageParams(BaseModel):
+    page: conint(ge=1) = 1
+    per_page: conint(ge=1, le=20) = 10
+
+
+class PagedResponseSchema(GenericModel, Generic[T]):
+    total: int
+    page: int
+    size: int
+    results: List[T]
