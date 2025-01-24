1c439f14899f2454cd53b4c07cf7372970fa1ecd
TarasFirst <firsttaras@gmail.com>
2025-01-24 09:11:39 +0100

    implement MovieDetailBase class

--- src/schemas/movies.py
+++ src/schemas/movies.py
@@ -1,7 +1,12 @@
 # Write your code here
+from datetime import datetime
 from typing import TypeVar, Generic, List
+
 from pydantic import BaseModel, conint, GenericModel
+
 T = TypeVar("T")
+
+
 class PageParams(BaseModel):
     page: conint(ge=1) = 1
     per_page: conint(ge=1, le=20) = 10
@@ -12,3 +17,13 @@ class PagedResponseSchema(GenericModel, Generic[T]):
     page: int
     size: int
     results: List[T]
+
+
+class MovieDetailBase(BaseModel):
+    name: str
+    date: datetime.date
+
+    class Config:
+        from_attributes = True
+
+
