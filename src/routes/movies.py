3adb4c5cabaa8d91972d54fa944fb34ad0a6ea64
TarasFirst <firsttaras@gmail.com>
2025-01-24 10:38:34 +0100

    Implement movie_router by id

--- src/routes/movies.py
+++ src/routes/movies.py
@@ -1,10 +1,18 @@
-from fastapi import APIRouter, Depends, HTTPException, Query
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
-from database import get_db, MovieModel
-
+from src.database.models import MovieModel
+from src.database.session import get_db
+from src.schemas.movies import MovieDetailResponseSchema
 
 router = APIRouter()
 
 
-# Write your code here
+@router.get("/movies/{movie_id}/", response_model=MovieDetailResponseSchema)
+def movie_router(movie_id: int, db: Session = Depends(get_db)):
+
+    movie = db.query(MovieModel).filter(MovieModel.id == movie_id).first()
+
+    if not movie:
+        raise HTTPException(status_code=404, detail="No movies found.")
+    return movie
